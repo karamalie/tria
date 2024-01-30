2a802a20ccf7190f98130274a1a0f2be90a9a0bc
karamalie <karam7898@gmail.com>
2024-01-31 00:16:27 +0800

    fix video logic

--- src/components/Login.tsx
+++ src/components/Login.tsx
@@ -107,17 +107,16 @@ const LoginPage = () => {
                 className="w-full h-full object-cover opacity-40"
               ></DotLottiePlayer>
             )}
-            {!connected ||
-              (!connecting && (
-                <video
-                  ref={videoRef}
-                  autoPlay
-                  loop
-                  muted
-                  className="w-full h-full object-cover opacity-70"
-                  src="https://s3-figma-videos-production-sig.figma.com/video/1147787743358927679/TEAM/3d5f/2bb9/-379c-4922-a6ff-00eb2f7c36d8?Expires=1707696000&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4&Signature=XH1PVnYjykDCg5WFil6S9alDSqsZB9hvcZ6vfWR8GRjpNX4oqANa3y5gzHC52jkO0~5wVzwJmsinYwq9xGQqoA7Y1hJT3K~yXIY~CS7-xEFndPfKBN1ifdnlLpHhhcCS73oIKFXfLXuHebdkNuLo67cR~XXAfSxJrEagtmiqK~-no-1O8-hZe9ufiDcbEKYUc-jPanKcryrCuAB1ilBX-zkAxf8oiwf0jq3M74CCg9whO6BQ0Ws2NNE96ncI~myiaTpqCKQUV~A3YsV8sqwxK~jg7Wx~GKY2I5Qcw91-pF8geEnhP8irFCQjvDW2g3hzpnLyeENtQfIbKlJ~CTLVkQ__" // Replace with your video path
-                ></video>
-              ))}
+            {(!connected || !connecting) && (
+              <video
+                ref={videoRef}
+                autoPlay
+                loop
+                muted
+                className="w-full h-full object-cover opacity-70"
+                src="https://s3-figma-videos-production-sig.figma.com/video/1147787743358927679/TEAM/3d5f/2bb9/-379c-4922-a6ff-00eb2f7c36d8?Expires=1707696000&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4&Signature=XH1PVnYjykDCg5WFil6S9alDSqsZB9hvcZ6vfWR8GRjpNX4oqANa3y5gzHC52jkO0~5wVzwJmsinYwq9xGQqoA7Y1hJT3K~yXIY~CS7-xEFndPfKBN1ifdnlLpHhhcCS73oIKFXfLXuHebdkNuLo67cR~XXAfSxJrEagtmiqK~-no-1O8-hZe9ufiDcbEKYUc-jPanKcryrCuAB1ilBX-zkAxf8oiwf0jq3M74CCg9whO6BQ0Ws2NNE96ncI~myiaTpqCKQUV~A3YsV8sqwxK~jg7Wx~GKY2I5Qcw91-pF8geEnhP8irFCQjvDW2g3hzpnLyeENtQfIbKlJ~CTLVkQ__" // Replace with your video path
+              ></video>
+            )}
           </div>
 
           {/* Text container with relative positioning to stack above the video */}
